1ff38ac885c92d3d7401322005f763dfe52b7cac
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-04-20 14:34:11 +0530

    ---------Models Folder

---
+++ LoginModel.cs
@@ -0,0 +1,12 @@
+﻿namespace coretest.Models
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Pass { get; set; }
+
+    }
+}
---
+++ MyModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace coretest.Models
+{
+    public class MyModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+         public string Email { get; set; }
+        [Required]
+        public string City { get; set; }
+  
+     
+
+    }
+}
